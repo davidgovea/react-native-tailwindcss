8e18057b68cfb3949da24f2f3a1212637f3e4bbd
David Govea <govea.d@gmail.com>
2019-05-07 14:44:54 -0700

    flex 1 behavior test

--- stubs/defaultConfig.stub.js
+++ stubs/defaultConfig.stub.js
@@ -351,7 +351,7 @@ module.exports = {
       current: 'currentColor',
     },
     flex: {
-      '1': '1 1 0%',
+      '1': '1',
       auto: '1 1 auto',
       initial: '0 1 auto',
       none: 'none',
