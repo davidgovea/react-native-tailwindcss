52a5284ad95c3532a8ac31be999f863c574d5bc9
David Govea <govea.d@gmail.com>
2019-05-15 01:58:43 -0700

    don't try to resolve custom config

--- util/configHandler.js
+++ util/configHandler.js
@@ -2,11 +2,11 @@ import resolveConfig from 'tailwindcss/resolveConfig';
 
 let file;
 
-try {
-    file = require('../../tailwind.config');
-} catch (ex) {
+// try {
+//     file = require('../../tailwind.config');
+// } catch (ex) {
     file = require('tailwindcss/stubs/simpleConfig.stub');
-}
+// }
 
 const {theme} = resolveConfig(file);
 
